f666ca3c3b77edec3f9407235dc506743a6b316b
jordiesc <jordi.escofet@gmail.com>
2024-12-21 09:06:32 +0100

    read file day2

---
+++ 2024/day2/dap.lua
@@ -0,0 +1,25 @@
+local ok, dap = pcall(require, 'dap')
+if not ok then
+    return
+end
+
+--
+-- See
+-- https://sourceware.org/gdb/current/onlinedocs/gdb.html/Interpreters.html
+-- https://sourceware.org/gdb/current/onlinedocs/gdb.html/Debugger-Adapter-Protocol.html
+dap.adapters.gdb = {
+    id = 'gdb',
+    type = 'executable',
+    command = 'gdb',
+    args = { '--quiet', '--interpreter=dap' },
+}
+
+dap.configurations.c = {
+    {
+        name = 'Run executable (GDB)',
+        type = 'gdb',
+        request = 'launch',
+        program = "day2-part1",
+console = "integratedTerminal",    }
+}
+
